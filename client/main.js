21fe5e45354123269ffd23cfb96ee65ed9ae2515
Tianqi Yan <YTQ@Tianqis-MacBook-Pro.local>
2016-10-08 22:40:09 +0800

    add vendor.getSaleRecord method for analytics

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 
 import './main.html';
 
+import { Orders } from '../lib/collections.js';
 
 Template.home.events({
     'click .logout': function(event){
@@ -11,3 +12,45 @@ Template.home.events({
         Meteor.logout();
     }
 });
+
+Meteor.methods({
+	'vendor.getSaleTrend': function(start, end, menu, scale, unit) {
+		// should return result in format {menuId: [number of order]}
+		// extract the date range into sub range
+		date_range = [];
+		start = moment(start);
+		end = moment(end);
+		temp_start = start;
+		while (temp_start < end) {
+			date_range.push([temp_start, temp_start.add(scale, unit)]);
+			temp_start = temp_start.add(scale, unit);
+		}
+
+		// extract the menu record based on date range
+		temp_result = {};
+		var menuId;
+		for (menuId in menu) {
+			temp_result[menuId] = Orders.find({'menuId' : menuId, 'orderTime' : {$gte:start, $lte:end}}, {'orderNum':1, 'orderTime':1}).fetch();
+		}
+
+		// finalize order amount based on sub range
+		result = {};
+		var menuId;
+		for (menuId in menu) {
+			result[menuId] = new Uint8Array(date_range.length);
+
+			var item;
+			for (item in temp_result['menuId']){
+				for (i=0; i<date_range.length; i++){
+					if (moment(item['orderTime']) < date_range[i][1] && moment(item['orderTime'] > date_range[i][0])) {
+						result[menuId] = result[menuId] + item['orderNum'];
+					}
+				}
+			}
+		}
+
+		return result;
+	},
+});
+
+
