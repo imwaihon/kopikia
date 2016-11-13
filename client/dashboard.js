d0dcae92d8b64ffca2efdcd18ee45635a6291009
imwaihon <imwaihon@gmail.com>
2016-11-13 19:25:33 +0800

    Added Drilldown Forecast Chart

--- client/dashboard.js
+++ client/dashboard.js
@@ -239,5 +239,173 @@ Template.vendorReportDashboard.helpers({
         });
       });
     });
-  }
+  },
+
+  'createReportChart3'() {
+    // HARD-CODED data
+    // Gather data:
+    // Use Meteor.defer() to craete chart after DOM is ready:
+    Meteor.defer(function() {
+      // Create standard Highcharts chart with options:
+      var colors = colors = Highcharts.getOptions().colors;
+      var categories = ["5 Nov", "6 Nov", "7 Nov", "8 Nov", "9 Nov"];
+
+      var data = [{
+              y: 95,
+              color: colors[0],
+              drilldown: {
+                  name: '5 Nov',
+                  categories: ["Chicken Rice", "Duck Rice", "Roasted Chicken Rice", "Braised Egg", "Beancurd"],
+                  data: [50,30,10,2,3],
+                  color: colors[0],
+                  x: 'Milk Tea'
+              }
+          }, {
+              y: 108,
+              color: colors[1],
+              drilldown: {
+                  name: '6 Nov',
+                  categories: ["Chicken Rice", "Duck Rice", "Roasted Chicken Rice", "Braised Egg", "Beancurd"],
+                  data: [40,50,10,5,3],
+                  color: colors[1]
+              }
+          }, {
+              y: 87,
+              color: colors[2],
+              drilldown: {
+                  name: '7 Nov',
+                  categories: ["Chicken Rice", "Duck Rice", "Roasted Chicken Rice", "Braised Egg", "Beancurd"],
+                  data: [20,30,10,7,12],
+                  color: colors[2]
+              }
+          }, {
+              y: 216,
+              color: colors[3],
+              drilldown: {
+                  name: '8 Nov',
+                  categories: ["Chicken Rice", "Duck Rice", "Roasted Chicken Rice", "Braised Egg", "Beancurd"],
+                  data: [120,50,10,12,24],
+                  color: colors[3]
+              }
+          }, {
+              y: 135,
+              color: colors[4],
+              drilldown: {
+                  name: '9 Nov',
+                  categories: ["Chicken Rice", "Duck Rice", "Roasted Chicken Rice", "Braised Egg", "Beancurd"],
+                  data: [50,30,40,12,13],
+                  color: colors[4]
+              }
+          }];
+      var chart = Highcharts.chart('reportChart3', {chart: {
+          type: 'column'
+        },
+        title: {
+          text: ''
+        },
+        xAxis: {
+            categories: categories
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Estimated Preparation Servings'
+          },
+          stackLabels: {
+            enabled: true,
+            style: {
+              fontWeight: 'bold',
+              color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+            }
+          }
+        },
+        legend: {
+          align: 'right',
+          x: -30,
+          verticalAlign: 'top',
+          y: 25,
+          floating: true,
+          backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+          borderColor: '#CCC',
+          borderWidth: 1,
+          shadow: false
+        },
+        tooltip: {
+            formatter: function() {
+                var point = this.point,
+                    s = this.x +': <b>'+ this.y +'</b><br/>';
+                if (point.drilldown) {
+                    s += 'Click to view '+ point.category +' breakdown';
+                } else {
+                    s += 'Click to return';
+                }
+                return s;
+            }
+        },
+        plotOptions: {
+            series: {
+                cursor: 'pointer',
+                point: {
+                    events: {
+                        click: function() {
+                            var drilldown = this.drilldown;
+                            var options;
+                            if (drilldown) { // drill down
+                                options = {
+                                    'name': drilldown.name,
+                                    'categories': drilldown.categories,
+                                    'data': drilldown.data,
+                                    'color': drilldown.color,
+                                    'type': 'column'
+                                };
+                            } else { // restore
+                                options = {
+                                    'name': name,
+                                    'categories': categories,
+                                    'data': data,
+                                    'type': 'column'
+                                };
+                            }
+                            setChart(options);
+                        }
+                    }
+                },
+                dataLabels: {
+                    enabled: true,
+                    color: 'black',
+                    style: {
+                        fontWeight: 'bold'
+                    },
+                    formatter: function() {
+                        return this.y;
+                    }
+                }
+            }
+        },
+
+        series: [{
+            showInLegend: false,
+            type: 'column',
+            name: name,
+            data: data,
+            color: 'white'
+        }],
+      });
+
+      function setChart(options) {
+          chart.series[0].remove(false);
+          chart.addSeries({
+              type: options.type,
+              name: options.name,
+              data: options.data,
+              color: options.color || 'white'
+          }, false);
+          chart.xAxis[0].setCategories(options.categories, false);
+          chart.redraw();
+      }
+    });
+
+  },
+
+
 });
